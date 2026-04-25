d516004623eddf976902b3b4afc87d03538fdec8
Bunonshilpo <137387512+Bunonshilpo@users.noreply.github.com>
2026-04-25 11:54:36 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,9 +6,7 @@ import { defineConfig, loadEnv } from 'vite';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
-    // এই লাইনটি যোগ করুন যাতে GitHub Pages সঠিক পাথ খুঁজে পায়
     base: '/bunonshilpo/', 
-    
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
